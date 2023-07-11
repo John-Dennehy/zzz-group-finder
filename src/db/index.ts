598da0744b7680d1f89935c26015d25a0cc879fe
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-07-11 15:11:25 +0100

    initial structure
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+import config from "@/db/config";
+import { groups } from "@/db/schema";
+
+// create the connection
+const connection = connect(config);
+
+const db = drizzle(connection);
+
+export const allGroups = await db
+  .select({
+    id: groups.id,
+    name: groups.name,
+    description: groups.description,
+  })
+  .from(groups);
---
+++ src/db/schema/groups.tsx
@@ -0,0 +1,27 @@
+import { InferModel } from "drizzle-orm";
+import {
+  mysqlTableCreator,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+const mysqlTable = mysqlTableCreator((name) => `groupfinder_${name}`);
+
+export const groups = mysqlTable("groups", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  when: varchar("when", { length: 256 }),
+  description: text("description"),
+  where: varchar("where", { length: 256 }),
+  url: varchar("url", { length: 256 }),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updated_at").defaultNow().notNull(),
+  verified_at: timestamp("verified_at"),
+  deleted_at: timestamp("deleted_at"),
+});
+
+export type Group = InferModel<typeof groups>;
+
+export default groups;
