81e5c3979318fa603d78d6ab6242c4948639b67c
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-03 22:14:24 +0100

    setup nextui
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link'
+import { Button } from '@nextui-org/button'; 
 
 export default function NotFound() {
 	return (
 		<div>
 			<h2>Page Not Found</h2>
-			<Link href="/">Return Home</Link>
+			<Button>
+				<Link href="/">Return Home</Link>
+			</Button>
 		</div>
 	)
 }
