245cea3b687a7ea555834648cfaec5b44900dd9c
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-04 22:40:27 +0100

    adds initial card component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,31 @@
 import { allGroups } from "@/db";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 
 export default async function Home() {
-	const data = await allGroups;
+  const data = await allGroups;
 
-	return (
-		<main>
-			<p className="text-lg text-blue-600">Admin Page</p>
-			<ul>
-				{data.map((group) => (
-					<li key={group.id}>
-						<p>{group.name}</p>
-						<p>{group.description}</p>
-						<p>{group.facebook}</p>
-					</li>
-				))}
-			</ul>
-		</main>
-	);
+  return (
+    <main className="container mx-auto">
+      <p className="text-lg text-blue-600">Admin Page</p>
+
+      <ul className="flex mx-auto gap-2 flex-wrap">
+        {data.map((group) => (
+          <li key={group.id}>
+            <Card
+              isBlurred
+              className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]"
+              shadow="sm"
+            >
+              <CardHeader>
+                <p className="text-lg text-primary">{group.name}</p>
+              </CardHeader>
+              <CardBody>
+                <p className="">{JSON.stringify(group)}</p>
+              </CardBody>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
