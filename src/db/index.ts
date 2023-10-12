2fbecbc8e8e4de9c2372e5a0f561d531acebc8e2
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-12 13:14:12 +0100

    adds Valibot
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import config from "@/db/config";
-import { groups } from "@/db/schema";
+import { GroupInsert, groups } from "@/db/schema";
+import { sql } from "drizzle-orm";
 
 // create the connection
 const connection = connect(config);
@@ -9,32 +10,10 @@ const connection = connect(config);
 const db = drizzle(connection);
 
 export const allGroups = await db
-  .select({
-    id: groups.id,
-    name: groups.name,
-    description: groups.description,
-    address: groups.address,
-    postCode: groups.postCode,
-    url: groups.url,
-    facebook: groups.facebook,
-    email: groups.email,
-    phone: groups.phone,
-    monStart: groups.timeMonStart,
-    monEnd: groups.timeMonEnd,
-    tueStart: groups.timeTueStart,
-    tueEnd: groups.timeTueEnd,
-    wedStart: groups.timeWedStart,
-    wedEnd: groups.timeWedEnd,
-    thuStart: groups.timeThuStart,
-    thuEnd: groups.timeThuEnd,
-    friStart: groups.timeFriStart,
-    friEnd: groups.timeFriEnd,
-    satStart: groups.timeSatStart,
-    satEnd: groups.timeSatEnd,
-    sunStart: groups.timeSunStart,
-    sunEnd: groups.timeSunEnd,
-    logoUrl: groups.logoUrl,
-    active: groups.active,
-    verified: groups.verifiedAt,
-  })
-  .from(groups);
+  .select()
+  .from(groups)
+  .where(sql`${groups.deletedAt} IS NULL AND ${groups.active} IS TRUE`);
+
+export async function insertGroup(group: GroupInsert) {
+  return db.insert(groups).values(group);
+}
