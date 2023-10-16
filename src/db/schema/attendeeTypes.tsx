aee929290ed80369ed5492a22c3766b875496ef3
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-16 22:29:46 +0100

    adds attendeeTypes schema
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

---
+++ src/db/schema/attendeeTypes.tsx
@@ -0,0 +1,35 @@
+import { mysqlTable } from "../../lib/mysqlTable";
+import {
+  InferSelectModel,
+  InferInsertModel,
+  relations,
+  sql,
+} from "drizzle-orm";
+import {
+  boolean,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
+import groupsToAttendeeTypes from "./groupsToAttendeeTypes";
+
+export const attendeeTypes = mysqlTable("attendee_types", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  description: text("description"),
+  active: boolean("active"),
+
+  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
+  deletedAt: timestamp("deleted_at"),
+});
+
+export const attendeeTypeRelations = relations(attendeeTypes, ({ many }) => ({
+  groupsToAttendeeTypes: many(groupsToAttendeeTypes),
+}));
+
+export type AttendeeType = InferSelectModel<typeof attendeeTypes>;
+export type AttendeeTypeInsert = InferInsertModel<typeof attendeeTypes>;
+
+export default attendeeTypes;
