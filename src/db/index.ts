d16b746af0a4168fc70a58d4b203bdea90bdcdb3
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-17 17:26:31 +0100

    updates group db functions
    - splits prev allGroups into allActiveGroups and allGroups
    - adds timestamp to 'updatedAt'
    - sets soft 'deleted' records to active: false
    - adds findGroupByName
    
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,32 +1,44 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import config from "@/db/config";
-import { GroupInsert, groups } from "@/db/schema";
-import { eq, isNull } from "drizzle-orm";
+import { GroupInsert, GroupUpdate, attendeeTypes, groups } from "@/db/schema";
+import { eq, isNull, sql } from "drizzle-orm";
 
 // create the connection
 const connection = connect(config);
 
 const db = drizzle(connection);
 
-export const allGroups = await db
+export const allActiveGroups = await db
   .select()
   .from(groups)
   .where(isNull(groups.deletedAt))
   .where(eq(groups.active, true));
 
+export const allGroups = await db
+  .select()
+  .from(groups)
+  .where(isNull(groups.deletedAt));
+
 export const insertGroup = async (group: GroupInsert) =>
   await db.insert(groups).values(group);
 
-export const updateGroup = async (id: number, group: GroupInsert) =>
-  await db.update(groups).set(group).where(eq(groups.id, id));
+export const updateGroup = async (id: number, group: GroupUpdate) =>
+  await db
+    .update(groups)
+    .set({ ...group, updatedAt: sql`CURRENT_TIMESTAMP` })
+    .where(eq(groups.id, id));
 
 export const deleteGroup = async (id: number) =>
   await db
     .update(groups)
-    .set({ deletedAt: new Date() })
+    .set({ active: false, deletedAt: sql`CURRENT_TIMESTAMP` })
     .where(eq(groups.id, id));
 
 export const findGroup = async (id: number) =>
   await db.select().from(groups).where(eq(groups.id, id));
+
+export const findGroupByName = async (name: string) =>
+  await db.select().from(groups).where(eq(groups.name, name));
+
 
