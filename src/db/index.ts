2a53e65e7a27817e8b7272ebc4ca239ea05d49b2
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-19 21:08:44 +0100

    refactor db connection
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,84 +1,12 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
-import config from "@/db/config";
-import {
-  AttendeeTypeInsert,
-  AttendeeTypeUpdate,
-  GroupInsert,
-  GroupUpdate,
-  attendeeTypes,
-  groups,
-} from "@/db/schema";
-import { eq, isNull, sql } from "drizzle-orm";
+import * as schema from "./schema";
+import { config } from "./config";
 
 // create the connection
 const connection = connect(config);
+export const db = drizzle(connection, { schema });
 
-const db = drizzle(connection);
-
-
-// Groups
-
-export const allActiveGroups = await db
-  .select()
-  .from(groups)
-  .where(isNull(groups.deletedAt))
-  .where(eq(groups.active, true));
-
-export const allGroups = await db
-  .select()
-  .from(groups)
-  .where(isNull(groups.deletedAt));
-
-export const insertGroup = async (group: GroupInsert) =>
-  await db.insert(groups).values(group);
-
-export const updateGroup = async (id: number, group: GroupUpdate) =>
-  await db
-    .update(groups)
-    .set({ ...group, updatedAt: sql`CURRENT_TIMESTAMP` })
-    .where(eq(groups.id, id));
-
-export const deleteGroup = async (id: number) =>
-  await db
-    .update(groups)
-    .set({ active: false, deletedAt: sql`CURRENT_TIMESTAMP` })
-    .where(eq(groups.id, id));
-
-export const findGroup = async (id: number) =>
-  await db.select().from(groups).where(eq(groups.id, id));
-
-export const findGroupByName = async (name: string) =>
-  await db.select().from(groups).where(eq(groups.name, name));
+export default db;
 
 
-// AttendeeTypes
-
-export const allActiveAttendeeTypes = await db
-  .select()
-  .from(attendeeTypes)
-  .where(isNull(attendeeTypes.deletedAt))
-  .where(eq(attendeeTypes.active, true));
-
-export const allAttendeeTypes = await db
-  .select()
-  .from(attendeeTypes)
-  .where(isNull(attendeeTypes.deletedAt));
-
-export const insertAttendeeType = async (attendeeType: AttendeeTypeInsert) =>
-  await db.insert(attendeeTypes).values(attendeeType);
-
-export const updateAttendeeType = async (
-  id: number,
-  attendeeType: AttendeeTypeUpdate
-) =>
-  await db
-    .update(attendeeTypes)
-    .set({ ...attendeeType, updatedAt: sql`CURRENT_TIMESTAMP` })
-    .where(eq(attendeeTypes.id, id));
-
-export const deleteAttendeeType = async (id: number) =>
-  await db
-    .update(attendeeTypes)
-    .set({ active: false, deletedAt: sql`CURRENT_TIMESTAMP` })
-    .where(eq(attendeeTypes.id, id));
---
+++ src/db/queries/getAllActiveGroups.ts
@@ -0,0 +1,62 @@
+import db from "@/db";
+
+export const getAllActiveGroups = await db.query.groups.findMany({
+  where(fields, { and, eq, isNull }) {
+    return and(isNull(fields.deletedAt), eq(fields.active, true));
+  },
+  columns: {
+    id: true,
+    name: true,
+    description: true,
+    postCode: true,
+    address: true,
+    active: true,
+    updatedAt: true,
+    verifiedAt: true,
+  },
+  with: {
+    groupsToAttendeeTypes: {
+      where(fields, { isNull }) {
+        return isNull(fields.deletedAt);
+      },
+      columns: {
+        attendeeTypeId: true,
+        updatedAt: true,
+      },
+      with: {
+        attendeeType: {
+          columns: {
+            name: true,
+            description: true,
+            active: true,
+          },
+        },
+      },
+    },
+    groupContactDetails: {
+      where(fields, { and, eq, isNull }) {
+        return and(isNull(fields.deletedAt), eq(fields.active, true));
+      },
+      columns: {
+        contactType: true,
+        contactValue: true,
+        forBooking: true,
+        forInformation: true,
+        updatedAt: true,
+      },
+    },
+    groupOpenHours: {
+      where(fields, { and, eq, isNull }) {
+        return and(isNull(fields.deletedAt), eq(fields.active, true));
+      },
+      columns: {
+        weekday: true,
+        start: true,
+        end: true,
+        updatedAt: true,
+      },
+    },
+  },
+});
+
+export default getAllActiveGroups;
