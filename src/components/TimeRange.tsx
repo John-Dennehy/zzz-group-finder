8757670ae72cf5d60067f067192eda0253c16c00
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2023-10-20 11:28:10 +0100

    updates TimeRange component
    Signed-off-by: John Dennehy <8093939+John-Dennehy@users.noreply.github.com>

--- src/components/TimeRange.tsx
+++ src/components/TimeRange.tsx
@@ -1,18 +1,38 @@
 import capitalise from "@/lib/capitalise";
 import formatTime from "@/lib/formatTime";
+import { Chip } from "@nextui-org/chip";
 
 export type TimeRangeProps = {
   start: string;
   end: string;
   weekday: "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
+  description: string | null;
 };
 
-export function TimeRange({ start, end, weekday }: TimeRangeProps) {
+export function TimeRange({
+  start,
+  end,
+  weekday,
+  description,
+}: TimeRangeProps) {
   if (!start || !end) {
     return null;
   }
 
+  const additionalInfo = description ? `  ${description}` : "";
   const formattedTime = `${formatTime(start)} - ${formatTime(end)}`;
 
-  return <p>{`${capitalise(weekday)}: ${formattedTime}`}</p>;
+  return (
+    <div className="flex gap-2">
+      <h4 className="w-12 font-bold">{capitalise(weekday)}:</h4>
+      <div className="flex gap-2">
+        <p>{formattedTime}</p>
+        {description && (
+          <Chip size="sm" variant="bordered">
+            {additionalInfo}
+          </Chip>
+        )}
+      </div>
+    </div>
+  );
 }
